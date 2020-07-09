fe9175a52432bf1eea5b3575891394f8ed063fbe
Diego Zepeda <diegomzepeda11@gmail.com>
2020-07-09 01:29:58 -0500

    Variables modified and clean code.

--- main.py
+++ main.py
@@ -14,60 +14,53 @@ print("-- Initializing Environment Variables --")
 load_dotenv()
 
 print("-- Initializing bot --")
-bot = commands.Bot(command_prefix='$')
-
-
+bot = commands.Bot(command_prefix='_')
 
 @bot.command()
 async def ping(ctx):
     print("-- Ping function called --")
-    await ctx.send('pong\n'*40)
+    await ctx.send('pong')
 
 @bot.event
 async def on_ready():
     game = discord.Game('Buscando la felicidad')
     await bot.change_presence(status=discord.Status.idle, activity=game)
-    print('My Ready is Body')
-    
+    print('-- Bot searcher ready --')
+
+
 @bot.command()
 async def google(ctx, *, search):
+    page_name = 'Google'
+    url ='https://www.google.com/search?'
+
+    print(f"Function {page_name} called: \n Search: {search} \n By: {ctx.author}")
+    await ctx.send(f'Searching in {page_name} for: {search}...')
+
     embed = discord.Embed(
-        title="Google Search Bot", 
-        description=f'Google search for "{search}".',
-        timestamp=datetime.datetime.utcnow(), 
+        title=f"{page_name} [Search Bot]",
+        description=f'Search for "{search}".',
+        timestamp=datetime.datetime.utcnow(),
         color=discord.Color.blue()
     )
 
-    query_string = parse.urlencode({'q': search})
-    url = 'https://www.google.com/search?' + query_string
-    agent_user = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
-    req = Request(url, headers={'User-Agent': agent_user})
+    query = parse.urlencode({'q': search})
+    url_query = url + query
+
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+    req = Request(url_query, headers={'User-Agent': user_agent})
     html_content = urlopen(req).read()
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    google_results = soup.select("div.g > div.rc")
-    if len(google_results) > 0:
-        for result in google_results:
+    results = soup.select("div.g > div.rc")
+    if len(results) > 0:
+        for result in results:
             link = result.find("a").attrs['href']
             tittle = result.find("h3").text
-            description = result.find("span",attrs={"class" : "st"}).text
-
+            description = result.find("span", attrs={"class": "st"}).text
             embed.add_field(name=tittle, value=f"{description} \n{link}", inline=False)
 
-    print(f"Function GOOGLE called: {search}")
-
-    """ 
-    youtube_results = soup.select("div.P94G9b")
-    if(len(youtube_results)>0):
-        for result in youtube_results:
-            print(result.prettify(), end="\n---------------")
-    """
-
-    # embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}",inline=False )
-    # embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
-    # embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
-    # embed.set_thumbnail(url=f"{ctx.guild.icon}")
     embed.set_thumbnail(url="https://rotulosmatesanz.com/wp-content/uploads/2017/09/2000px-Google_G_Logo.svg_.png")
+
     await ctx.send(embed=embed)
 
 token = os.getenv("DISCORD_TOKEN")
