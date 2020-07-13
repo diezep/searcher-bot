0ef69676c73b908a41e517d46c111af021136473
Diego Zepeda <diegomzepeda11@gmail.com>
2020-07-13 05:07:24 -0500

    Google cog added.

---
+++ cogs/Google.py
@@ -0,0 +1,51 @@
+import datetime
+from urllib import parse
+
+import discord
+import requests
+from bs4 import BeautifulSoup
+from discord.ext import commands
+
+
+class Google(commands.Cog):
+
+    page_name = 'Google'
+    url = 'https://www.google.com/search?'
+    url_param = 'q'
+    embed_image = 'https://i.dlpng.com/static/png/6654190_preview.png'
+
+    def __init__(self, bot, headers):
+        self.bot = bot
+        self.headers = headers
+
+    @commands.command()
+    async def google(self, ctx, *, search):
+        await ctx.send(f'Searching in {self.page_name} for: {search}...')
+        # print_log(page_name, search, ctx.author)
+
+        embed = discord.Embed(
+            title=f"{self.page_name} [Search Bot]",
+            description=f'Search for "{search}".',
+            timestamp=datetime.datetime.utcnow(),
+            color=discord.Color.blue()
+        )
+
+        query = parse.urlencode({self.url_param: search})
+        url_query = self.url + query
+
+        res = requests.get(url_query, headers=self.headers, allow_redirects=False)
+        html_content = res.content
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        results = soup.select("div.g > div.rc")
+
+        if len(results) > 0:
+            for result in results:
+                link = result.find("a").attrs['href']
+                tittle = result.find("h3").text
+                description = result.find("span", attrs={"class": "st"}).text
+                embed.add_field(name=tittle, value=f"{description} \n{link}", inline=False)
+
+        embed.set_thumbnail(url=self.embed_image)
+
+        await ctx.send(embed=embed)
--- main.py
+++ main.py
@@ -2,14 +2,12 @@
 
 import datetime
 import os
-from urllib import parse
 
 import discord
-import requests
-from bs4 import BeautifulSoup
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from cogs.Google import Google
 from cogs.StardewValley import StardewValley
 from cogs.Terraria import Terraria
 
@@ -38,40 +36,8 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.idle, activity=game)
     print('-- Bot searcher ready --')
 
-@bot.command()
-async def google(ctx, *, search):
-    page_name = 'Google'
-    url ='https://www.google.com/search?'
-    url_param = 'q'
-    
-    await ctx.send(f'Searching in {page_name} for: {search}...')
-    print_log(page_name, search, ctx.author)
-
-    embed = discord.Embed(
-        title=f"{page_name} [Search Bot]",
-        description=f'Search for "{search}".',
-        timestamp=datetime.datetime.utcnow(),
-        color=discord.Color.blue()
-    )
-
-    query = parse.urlencode({url_param: search})
-    url_query = url + query
-
-    res = requests.get(url_query, headers=headers, allow_redirects=False)
-    html_content = res.content
-    soup = BeautifulSoup(html_content, 'html.parser')
-
-    results = soup.select("div.g > div.rc")
-    if len(results) > 0:
-        for result in results:
-            link = result.find("a").attrs['href']
-            tittle = result.find("h3").text
-            description = result.find("span", attrs={"class": "st"}).text
-            embed.add_field(name=tittle, value=f"{description} \n{link}", inline=False)
-
-    embed.set_thumbnail(url="https://rotulosmatesanz.com/wp-content/uploads/2017/09/2000px-Google_G_Logo.svg_.png")
-
-    await ctx.send(embed=embed)
+# Google command and functions.
+bot.add_cog(Google(bot, headers))
 
 # Stardew Valley command and functions.
 bot.add_cog(StardewValley(bot, headers))
