1fd9e5bd9648d43f5e835c02dc75d473c389fb2b
Diego Zepeda <diegomzepeda11@gmail.com>
2020-07-14 03:41:55 -0500

    Experimental function testing in server.

--- utils/webss.py
+++ utils/webss.py
@@ -1,8 +1,10 @@
 import os
 from io import BytesIO
+from discord import File
 
 from PIL import Image
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class WebSS():
@@ -11,23 +13,30 @@ class WebSS():
         self.url = url
 
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        chrome_options.add_argument('log-level=3')
+        # chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--start-fullscreen")
 
-    def ofElement(self, xpath):
+        self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        self.driver.set_window_size(2048, 1365)
+
+    def ofElement(self, search, xpath):
 
         self.driver.get(self.url)
+        
+        ActionChains(self.driver).move_to_element(self.driver.find_element_by_xpath(xpath)).perform()
         _image = self.driver.find_element_by_xpath(xpath).screenshot_as_png
-        image = Image.open(BytesIO(_image))
+        imageBytes = BytesIO(_image)
 
+        file = File(imageBytes, filename=f"{search}.png")
         # element = driver.find_element_by_xpath("//div[@id='hplogo']")
         #
         # location = element.location
         # size = element.size
-        #
+        #.
         # driver.save_screenshot("/data/image.png")
         #
         # x = location['x']
@@ -37,4 +46,7 @@ class WebSS():
         #
         # im = Image.open('/data/WorkArea/image.png')
         # im = im.crop((int(x), int(y), int(width), int(height)))
-        return image
+        return file
+
+    def close(self):
+        self.driver.close()
