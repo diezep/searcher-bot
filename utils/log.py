4e4d8b018f9a442af6c7255062b0fd28775680a1
Diego Zepeda <diegomzepeda11@gmail.com>
2020-07-15 05:20:15 -0500

    Removed files, this branch is only for Github Pages

--- utils/log.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-def print_log(page_name, search, author):
-    print(f"""Function {page_name} called:  
-        Search: {search} 
-        By: {author}
-        Timestamp: {datetime.datetime.utcnow()} """)
-
