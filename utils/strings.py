62a83b40b5df0463e4ee138552e40cbd30a4d37f
Diego Zepeda <diegomzepeda11@gmail.com>
2020-07-19 05:32:00 -0500

    Added string function to remove accents.

--- utils/strings.py
+++ utils/strings.py
@@ -1,9 +1,20 @@
-def compare_strings(search, tittle):
+import unicodedata
+
+def compare_strings(search, title):
+    
     a, b = 'áéíóúü','aeiouu'
     
-    search =  search.maketrans(a, b).replace(' ','')
-    tittle =  tittle.maketrans(a, b).replace(' ','')
+    search =  remove_accents(search)
+    title =  remove_accents(title)
     
-    if search == tittle: return True
+    search =  search.lower()
+    title =  title.lower()
+    
+    if search == title: return True
     else: return False
-        
+
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    only_ascii = nfkd_form.encode('ASCII', 'ignore')
+    return only_ascii
